9b5c82d0d5eb9321335e657a40839f1cb552488e
Andrew Marder <andrew.marder@caveon.com>
2021-09-09 11:14:18 -0400

    Set up an .emacs-profiles.el file

---
+++ .emacs-profiles.el
@@ -0,0 +1,4 @@
+;; https://github.com/plexus/dotfiles/blob/master/.emacs-profiles.el
+
+(("default" . ((user-emacs-directory . "~/emacs-profiles/kjhealy")))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs"))))
